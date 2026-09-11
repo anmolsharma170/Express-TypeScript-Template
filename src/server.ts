91e66b6f7c2778ce64ffd76e88861b4d15847b8d
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-09-11 13:16:12 +0530

    added basic ts code with express

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+const PORT = 3001;
+app.get('/ping',(req,res)=>{
+    res.send('pong');
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
