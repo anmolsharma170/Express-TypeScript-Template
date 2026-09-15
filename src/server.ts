33f2f4ab12d64a12f8cb325180d43f2cfd434a5c
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-09-15 18:59:03 +0530

    added api version

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import {serverConfig} from './config/index.js'
-import pingRouter  from './routers/ping.router.js';
+import v1Router from './routers/v1/index.router.js';
+import v2Router from './routers/v2/index.router.js';
 const app = express();
 
-app.use(pingRouter); // registering all the routes and their corresponding routes with our app server object
+app.use('/api/v1',v1Router); // registering all the routes and their corresponding routes with our app server object
+app.use('/api/v2',v2Router);
 
-console.log(`Envirement variables loaded`);
 app.listen(serverConfig.PORT,()=>{
     console.log(`Server is running on port ${serverConfig.PORT}`);
 });
